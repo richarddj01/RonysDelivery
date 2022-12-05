fc9b6e3dd70536b1608a2a82a1e282e700de637d
JoseAbrego1 <99070853+JoseAbrego1@users.noreply.github.com>
2022-12-04 19:15:53 -0600

    Insertar_Pedido

--- app/src/main/java/com/example/ronysdelivery/PH_Pizza_Personal_Doble.java
+++ app/src/main/java/com/example/ronysdelivery/PH_Pizza_Personal_Doble.java
@@ -2,17 +2,22 @@ package com.example.ronysdelivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.RadioButton;
 
 public class PH_Pizza_Personal_Doble extends AppCompatActivity {
     private String precioConComplemento;
     private boolean seleccionado;
     private TextView precio;
     private TextView total;
-    private TextView lbl_cantidad;
+    private TextView lbl_cantidad,lbl_titulo,lbl_total,lbl_NombreCombo;
     private int cantidad;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -144,5 +149,34 @@ public class PH_Pizza_Personal_Doble extends AppCompatActivity {
 
             total.setText(herramientas.calcularTotal(view, price));
         }
+    }
+    public void InsertarPedido(View view)
+    {
+        lbl_titulo = (TextView) findViewById(R.id.lbl_titulo);
+        lbl_cantidad = (TextView) findViewById(R.id.lbl_cantidad);
+        precio = (TextView)  findViewById(R.id.lbl_comboPrecio);
+        String correo;
+        AdminSQLiteOpen Bd = new AdminSQLiteOpen(this, "RonysDelivery", null,1);//objeto clase
+        SQLiteDatabase BasedeDatos = Bd.getWritableDatabase();//escritura BD
+
+        // Restaurante = ob_codigo.getText().toString();//obtenemos el codigo ingresado en el teclado
+        String NombreCombo = lbl_titulo.getText().toString();
+        String Total = precio.getText().toString();
+        String Cantidad = lbl_cantidad.getText().toString();
+        //String NombreCom = lbl_NombreCombo.getText().toString();
+        //String SubTotal = ob_campus.getText().toString();
+
+        correo = herramientas.Correo();
+        ContentValues registro = new ContentValues();
+        //registro.put("PedCodigo",1);
+        registro.put("Combo", NombreCombo);
+        registro.put("PedCantidad", Integer.valueOf(Cantidad));
+        registro.put("PedPrecio", Float.parseFloat(Total.substring(2)));
+        registro.put("UsuCorreo", correo);
+
+        //NombreCombo
+
+        Toast.makeText(this,"DATOS GUARDADOS", Toast.LENGTH_SHORT).show();
+        BasedeDatos.insert("Pedidos", null, registro);
     }
 }
